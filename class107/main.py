b388398971868ccadefb479893e1be19813e47b0
Shlok3000 <71341641+Shlok3000@users.noreply.github.com>
2022-02-11 19:48:46 -0500

    Add files via upload

---
+++ class107/main.py
@@ -0,0 +1,13 @@
+import csv
+import pandas as pd
+import plotly.graph_objects as go
+
+df = pd.read_csv("data.csv")
+
+
+student_df = df.loc[df["student_id"] == "TRL_abc"]
+#print(student_df)
+fig = go.Figure(go.Bar(x = student_df.groupby("level")["attempt"].mean(),
+y = ["level 1", "level 2", "level 3", "level 4"],
+orientation = "h"))
+fig.show()
